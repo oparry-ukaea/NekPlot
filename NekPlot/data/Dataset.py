aff7f20b557179195d52b1d96301fc138879a66f
Owen Parry <owen.parry@ukaea.uk>
2022-08-04 14:29:03 +0100

    Deleted an unused class.

--- NekPlot/data/Dataset.py
+++
@@ -1,7 +0,0 @@
-
-class Dataset():
-    def __init__(self,label,data_source,style_generator):
-        self.label = label
-        self.src = data_source
-        self.sty_gen = style_generator
-    
