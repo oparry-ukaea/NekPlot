2878cbff52549d9d5983e11093bdc9eff95458c4
Owen Parry <owen.parry@ukaea.uk>
2022-10-17 09:39:01 +0100

    Add skeleton setup.py and a missing __init__.py to allow use as a package.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NekPlot',
+    version='0.1.0',
+    packages=find_packages(include=['NekPlot', 'NekPlot.*']),
+    install_requires=['matplotlib>=3.5.2','numpy>=1.22.4','pandas>=1.4.3']
+)
