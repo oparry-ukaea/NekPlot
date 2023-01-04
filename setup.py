b9f36da11aba2fa9fd08634f710bb1a9a19b8ddb
Owen Parry <owen.parry@ukaea.uk>
2023-01-04 15:07:38 +0000

    Update version to 0.2.1 .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='NekPlot',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(include=['NekPlot', 'NekPlot.*']),
     install_requires=['matplotlib>=3.5.2','numpy>=1.22.4','pandas>=1.4.3']
 )
